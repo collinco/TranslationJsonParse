118c14f4165cddaa5fddfff7885b4f97be841e91
Cole <ncollins@utexas.edu>
2017-11-30 00:08:57 -0800

    Created called, moved logic to functions, made output a tad cleaner. Lots of changes since the initial program was a mess. Still a mess but gettting better

---
+++ classes/steps.py
@@ -0,0 +1,12 @@
+class Step1and2:
+    def __init__(self):
+        self.wordCount = 0
+        self.text = ""
+        self.key = ""
+        self.words = []
+
+
+class Step3:
+    def __init__(self):
+        self.numberOfOccurences = 0
+        self.filesFoundin = []
